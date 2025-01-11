e13a9d0e2372f12d203f08e21003485ebcd66131
Marko Gusic <markoggusic@gmail.com>
2025-01-11 12:40:08 +0100

    change undeclared terraform name for aws_eip

--- iac/gateways.tf
+++ iac/gateways.tf
@@ -10,7 +10,7 @@ resource "aws_internet_gateway" "igw" {
 
 # NAT gateway 
 resource "aws_nat_gateway" "nat" {
-  allocation_id = aws_eip.example.id
+  allocation_id = aws_eip.eip.id
   subnet_id     = aws_subnet.public.id
 
   tags = {
